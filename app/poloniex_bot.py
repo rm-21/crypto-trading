5e270c1f9b3b81e777baac4b53672ff8dcc244b3
rm-21 <mittalrishabh181@gmail.com>
2022-04-07 21:53:21 +0530

    Added all pairs function for identify_pairs.py
    Need to improve it

--- app/poloniex_bot.py
+++ app/poloniex_bot.py
@@ -1,5 +1,6 @@
 import sys
 from pprint import pprint
+import json
 
 sys.path.append("..")
 from modules.data.poloniex.poloniex_api import Poloniex as pl
@@ -16,25 +17,54 @@ if __name__ == "__main__":
     coin_list = data_obj.get_coins_tradeable
 
     ## Pairs
-    trio = IdentifyPairs(
-        coin_list, paired_order=["USDT_BTC", "USDT_ETH", "BTC_ETH"]
-    ).get_tradeable_trio
+    # trio = IdentifyPairs(
+    #     coin_list, paired_order=["USDT_BTC", "USDT_ETH", "BTC_ETH"]
+    # ).get_tradeable_trio
 
     ## Trio details
-    trio_details = data_obj.get_details_for_trio(trio)
-    trio_prices = data_obj.get_price_for_trio(trio)
+    # trio_details = data_obj.get_details_for_trio(trio)
+    # trio_prices = data_obj.get_price_for_trio(trio)
 
     # print(trio_prices)
 
     ## Main
-    obj1 = SurfaceArb(trio, trio_prices, 100, "USDT")
-    pprint(obj1.get_trade_logs)
-    print()
+    # ustd_btc_last = float(data_obj.json_resp["USDT_BTC"]["last"])
+    # usdt_eth_last = float(data_obj.json_resp["USDT_ETH"]["last"])
+    # btc_eth_last = float(data_obj.json_resp["BTC_ETH"]["last"])
 
-    obj2 = SurfaceArb(trio, trio_prices, 1, "BTC")
-    pprint(obj2.get_trade_logs)
-    print()
+    # multiplication_ = ustd_btc_last * btc_eth_last
+    # print(multiplication_, usdt_eth_last, multiplication_ - usdt_eth_last)
 
-    obj3 = SurfaceArb(trio, trio_prices, 50, "ETH")
-    pprint(obj3.get_trade_logs)
-    print()
+    # obj1 = SurfaceArb(trio, trio_prices, 10000, "USDT")
+    # pprint(obj1.get_trade_logs)
+    # print()
+
+    # obj2 = SurfaceArb(trio, trio_prices, 10, "BTC")
+    # pprint(obj2.get_trade_logs)
+    # print()
+
+    # obj3 = SurfaceArb(trio, trio_prices, 50, "ETH")
+    # pprint(obj3.get_trade_logs)
+    # print()
+
+    # Get Structured Pairs
+    with open("structured_triangular_pairs.json") as json_file:
+        structured_pairs = json.load(json_file)
+
+    trio_list = []
+    for t_pair in structured_pairs:
+        trio_list.append(t_pair["combined"].split(","))
+
+    for t in trio_list:
+        obj1 = SurfaceArb(t, data_obj.get_price_for_trio(t), 1, t[0].split("_")[0])
+        df = obj1.get_trade_logs
+        if len(df[df["profit"] > 0]) > 0:
+            print("-" * 100)
+            print(obj1.trio)
+            print(df)
+            print("-" * 100)
+
+# BTC   ETH
+# USTD  BTC
+#
+# ETH/USDT
--- modules/strategy/identify_pairs.py
+++ modules/strategy/identify_pairs.py
@@ -1,7 +1,15 @@
 from itertools import permutations
+import json
+
 
 class IdentifyPairs:
-    def __init__(self, tradeable_pairs:list, individual_currencies:list = None, paired_order:list = None, find_all:bool = False):
+    def __init__(
+        self,
+        tradeable_pairs: list,
+        individual_currencies: list = None,
+        paired_order: list = None,
+        find_all: bool = False,
+    ):
         self.tradeable_pairs = tradeable_pairs
         self.individual_currencies = individual_currencies
         self.paired_order = paired_order
@@ -10,7 +18,11 @@ class IdentifyPairs:
         ## Final Pairs
         # 1. If paired_order is there, validate it and see if it is present in the tradeable_pairs list
         # 2. If 1 is false, and individual currencies are present, create trio and check as in 1
-        self._tradeable_trio = self.paired_order if self._check_paired_order() else self._create_tradeable_trio_from_individual()
+        self._tradeable_trio = (
+            self.paired_order
+            if self._check_paired_order()
+            else self._create_tradeable_trio_from_individual()
+        )
 
         ## Complete List of tradeable pairs
         # If Final Pairs is None OR find_all == True
@@ -31,18 +43,20 @@ class IdentifyPairs:
         Validate a currency pair by checking if "_" is present and two currencies on each side are present.
         """
         if ("_" not in item) or (len(item.split("_")) != 2):
-            raise ValueError(f"{item}: Invalid Pair. Pair should be of the form <Currency>_<Currency>.")
+            raise ValueError(
+                f"{item}: Invalid Pair. Pair should be of the form <Currency>_<Currency>."
+            )
 
     def _check_paired_order(self):
         """
         Checks whether the paired_order is present in the tradeable_pairs list.
-        
+
         Returns:
         * True: If present
         """
         # O(3*n) ~ O(n)
-        if (self.paired_order):
-            if (len(self.paired_order) == 3):
+        if self.paired_order:
+            if len(self.paired_order) == 3:
                 for pair in self.paired_order:
                     IdentifyPairs._validate_paired_currency(pair)
                     if pair not in self.tradeable_pairs:
@@ -54,19 +68,19 @@ class IdentifyPairs:
 
     def _create_tradeable_trio_from_individual(self):
         """
-        If paired_order: 
+        If paired_order:
         * is None
 
-        Returns: 
+        Returns:
         * tradeable_trio: if individual_currencies is not None
         * False: otherwise
         """
         trio = []
         if (self.paired_order == None) and (self.individual_currencies):
-            all_possible_pairs =  list(permutations(self.individual_currencies, 2))
+            all_possible_pairs = list(permutations(self.individual_currencies, 2))
             self.paired_order = list(map(lambda x: "_".join(x), all_possible_pairs))
 
-            while (len(trio) < 3):
+            while len(trio) < 3:
                 for pair in self.paired_order:
                     IdentifyPairs._validate_paired_currency(pair)
                     if pair in self.tradeable_pairs:
@@ -74,10 +88,92 @@ class IdentifyPairs:
                 return trio
         return False
 
-    def _create_all_pairs(self):
+    def _create_all_pairs(self, dump=True):
         """
         To be implemented
         """
-        if ((self.paired_order == None or self.paired_order == False) and (self.individual_currencies == None)) or (self.find_all):
-            pass
-        return None
+        if (
+            (self.paired_order == None or self.paired_order == False)
+            and (self.individual_currencies == None)
+        ) or (self.find_all):
+            # Declare Variables
+            triangular_pairs_list = []
+            remove_duplicates_list = []
+            pairs_list = self.tradeable_pairs[0:]
+
+            # Get Pair A
+            for pair_a in pairs_list:
+                pair_a_split = pair_a.split("_")
+                a_base = pair_a_split[0]
+                a_quote = pair_a_split[1]
+
+                # Assign A to a Box
+                a_pair_box = [a_base, a_quote]
+
+                # Get Pair B
+                for pair_b in pairs_list:
+                    pair_b_split = pair_b.split("_")
+                    b_base = pair_b_split[0]
+                    b_quote = pair_b_split[1]
+
+                    # Check Pair B
+                    if pair_b != pair_a:
+                        if b_base in a_pair_box or b_quote in a_pair_box:
+
+                            # Get Pair C
+                            for pair_c in pairs_list:
+                                pair_c_split = pair_c.split("_")
+                                c_base = pair_c_split[0]
+                                c_quote = pair_c_split[1]
+
+                                # Count the number of matching C items
+                                if pair_c != pair_a and pair_c != pair_b:
+                                    combine_all = [pair_a, pair_b, pair_c]
+                                    pair_box = [
+                                        a_base,
+                                        a_quote,
+                                        b_base,
+                                        b_quote,
+                                        c_base,
+                                        c_quote,
+                                    ]
+
+                                    counts_c_base = 0
+                                    for i in pair_box:
+                                        if i == c_base:
+                                            counts_c_base += 1
+
+                                    counts_c_quote = 0
+                                    for i in pair_box:
+                                        if i == c_quote:
+                                            counts_c_quote += 1
+
+                                    # Determining Triangular Match
+                                    if (
+                                        counts_c_base == 2
+                                        and counts_c_quote == 2
+                                        and c_base != c_quote
+                                    ):
+                                        combined = pair_a + "," + pair_b + "," + pair_c
+                                        unique_item = "".join(sorted(combine_all))
+
+                                        if unique_item not in remove_duplicates_list:
+                                            match_dict = {
+                                                "a_base": a_base,
+                                                "b_base": b_base,
+                                                "c_base": c_base,
+                                                "a_quote": a_quote,
+                                                "b_quote": b_quote,
+                                                "c_quote": c_quote,
+                                                "pair_a": pair_a,
+                                                "pair_b": pair_b,
+                                                "pair_c": pair_c,
+                                                "combined": combined,
+                                            }
+                                            triangular_pairs_list.append(match_dict)
+                                            remove_duplicates_list.append(unique_item)
+
+            with open("structured_triangular_pairs.json", "w") as fp:
+                json.dump(triangular_pairs_list, fp)
+
+            return triangular_pairs_list
