189202c76fa99a02624e388b04a04cd09e036763
rm-21 <mittalrishabh181@gmail.com>
2022-04-07 20:03:39 +0530

    Removed pycache and refactored poloniex_bot.py

--- app/poloniex_bot.py
+++ app/poloniex_bot.py
@@ -10,15 +10,10 @@ from modules.strategy.surface_arb import SurfaceArb
 coin_price_url = "https://poloniex.com/public?command=returnTicker"
 
 
-class MainPoliniex(pl):
-    def __init__(self, URL: str):
-        super().__init__(URL)
-
-
 if __name__ == "__main__":
-    ## Coin list
-    main_obj = MainPoliniex(coin_price_url)
-    coin_list = main_obj.get_coins_tradeable
+    data_obj = pl(coin_price_url)
+
+    coin_list = data_obj.get_coins_tradeable
 
     ## Pairs
     trio = IdentifyPairs(
@@ -26,31 +21,20 @@ if __name__ == "__main__":
     ).get_tradeable_trio
 
     ## Trio details
-    trio_details = main_obj.get_details_for_trio(trio)
-    trio_prices = main_obj.get_price_for_trio(trio)
-    # print(trio_details)
-    print(trio_prices)
+    trio_details = data_obj.get_details_for_trio(trio)
+    trio_prices = data_obj.get_price_for_trio(trio)
 
-    graph_obj = SurfaceArb(trio=trio, trio_prices=trio_prices)
-    quotes = graph_obj._get_quote_to_use("USDT")
-    # print(quotes)
-    ## Conversion check
-    # btc_to_usd = Conversion.currency_conversion(
-    #     100,
-    #     trio_details["pair_1_base"],
-    #     trio_details["pair_1_quote"],
-    #     trio_prices["pair_1_bid"],
-    #     trio_prices["pair_1_ask"],
-    #     "forward",
-    # )
-    # print(btc_to_usd)
+    # print(trio_prices)
 
-    # usd_to_btc = Conversion.currency_conversion(
-    #     btc_to_usd["new_amount"],
-    #     trio_details["pair_1_base"],
-    #     trio_details["pair_1_quote"],
-    #     trio_prices["pair_1_bid"],
-    #     trio_prices["pair_1_ask"],
-    #     "reverse",
-    # )
-    # print(usd_to_btc)
+    ## Main
+    obj1 = SurfaceArb(trio, trio_prices, 100, "USDT")
+    pprint(obj1.get_trade_logs)
+    print()
+
+    obj2 = SurfaceArb(trio, trio_prices, 1, "BTC")
+    pprint(obj2.get_trade_logs)
+    print()
+
+    obj3 = SurfaceArb(trio, trio_prices, 50, "ETH")
+    pprint(obj3.get_trade_logs)
+    print()
