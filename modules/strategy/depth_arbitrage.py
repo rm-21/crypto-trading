4c16f17f5eabc049dea4cde3bd1526311fc4679b
rm-21 <mittalrishabh181@gmail.com>
2022-04-08 10:49:05 +0530

    Cleared code for depth

--- modules/strategy/depth_arbitrage.py
+++ modules/strategy/depth_arbitrage.py
@@ -2,8 +2,4 @@ from surface_arb import SurfaceArb
 
 
 class DepthArb(SurfaceArb):
-    def __init__(
-        self, trio: list, trio_prices: dict, init_amount: float, init_currency: str
-    ):
-        super().__init__(trio, trio_prices, init_amount, init_currency)
-        DepthArb.TRADES_LOG = self.get_trade_logs
+    pass
