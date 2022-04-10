cb4d1a8517e5a2e9018fe6fb7a6b920262ec8916
rm-21 <mittalrishabh181@gmail.com>
2022-04-10 16:05:46 +0530

    Edited identify_pairs.py to remove unnecessary code

--- modules/strategy/identify_pairs.py
+++ modules/strategy/identify_pairs.py
@@ -5,21 +5,9 @@ import pandas as pd
 
 
 class IdentifyPairs:
-    def __init__(self, paired_order: dict = None,
-                 currency_list: list = None, exchange_list: list = None,
-                 find_all: bool = False):
-
-        # Option 1
+    def __init__(self, paired_order: dict = None):
         self.paired_order = paired_order
 
-        # Option 2
-        self.currency_list = currency_list
-        self.exchange_list = exchange_list
-
-        # Choice
-        self.find_all = find_all
-
-        # If paired order is given, check if it is valid
         self._trio_details = None
         self._get_trio_details()
 
@@ -27,15 +15,9 @@ class IdentifyPairs:
     def get_tradeable_trio(self):
         return self._trio_details
 
-    @property
-    def get_all_tradeable_trios(self):
-        pass
-
     def _get_trio_details(self):
         if self.paired_order:
             self._trio_details = self._validate_paired_order()
-        else:
-            self._trio_details = self._get_paired_order()
 
     def _validate_paired_order(self):
         with ThreadPoolExecutor(max_workers=10) as pool:
@@ -49,23 +31,12 @@ class IdentifyPairs:
             raise ValueError("Currency pairs are not correct. Please check that there is 2 of each currency.")
         return details
 
-    def _get_paired_order(self):
-        """
-        To be implemented
-        """
-        pass
-
 
 if __name__ == "__main__":
     from modules.data.platform.btcmarkets import BTCMarkets
     from modules.data.platform.independent_reserve import IndependentReserve
     from modules.data.platform.oanda import Oanda
 
-    # cur_dict1 = {
-    #     "AUD_SGD": Oanda,
-    #     "BTC_AUD": BTCMarkets,
-    #     "BTC_SGD": IndependentReserve
-    # }
     cur_dict1 = {
         "AUD_SGD": Oanda,
         "BTC_AUD": BTCMarkets,
