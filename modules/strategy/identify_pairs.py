74e7dd7ca2c130ca6bd21f376d4606b6ce1075d2
rm-21 <mittalrishabh181@gmail.com>
2022-04-10 12:16:57 +0530

    Reformatted identify_pairs.py
    Need to implement for generalsitic. Completed for individual

--- modules/strategy/identify_pairs.py
+++ modules/strategy/identify_pairs.py
@@ -5,16 +5,16 @@ import pandas as pd
 
 
 class IdentifyPairs:
-    def __init__(self, currency_list: list = None, exchange_list: list = None,
-                 paired_order: dict = None,
+    def __init__(self, paired_order: dict = None,
+                 currency_list: list = None, exchange_list: list = None,
                  find_all: bool = False):
 
         # Option 1
-        self.currency_list = currency_list
-        self.exchange_list = exchange_list
+        self.paired_order = paired_order
 
         # Option 2
-        self.paired_order = paired_order
+        self.currency_list = currency_list
+        self.exchange_list = exchange_list
 
         # Choice
         self.find_all = find_all
@@ -29,7 +29,7 @@ class IdentifyPairs:
 
     @property
     def get_all_tradeable_trios(self):
-        return self._tradeable_all_pairs
+        pass
 
     def _get_trio_details(self):
         if self.paired_order:
@@ -45,6 +45,9 @@ class IdentifyPairs:
         return details
 
     def _get_paired_order(self):
+        """
+        To be implemented
+        """
         pass
 
 
@@ -59,11 +62,12 @@ if __name__ == "__main__":
         "BTC_SGD": IndependentReserve
     }
 
-    # When paired object is given
+    ## When paired object is given
     obj1 = IdentifyPairs(paired_order=cur_dict1)
     trio_details = obj1.get_tradeable_trio
     print(trio_details)
 
-    # When paired object is not given
-    list_cur = ["AUD", "SGD", "BTC"]
-    list_exch = [Oanda, IndependentReserve, BTCMarkets]
+    ## When paired object is not given
+    # list_cur = ["AUD", "SGD", "BTC"]
+    # print(list(permutations(list_cur, 2)))
+    # list_exch = [Oanda, IndependentReserve, BTCMarkets]
