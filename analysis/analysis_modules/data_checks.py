a4512ba39c00515876541647f8ccfca3b65940e8
rm-21 <mittalrishabh181@gmail.com>
2022-04-19 08:14:10 +0530

    Refactor + Exploration

--- analysis/analysis_modules/data_checks.py
+++ analysis/analysis_modules/data_checks.py
@@ -1,23 +1,7 @@
 import pandas as pd
 import matplotlib.pyplot as plt
+from analysis.analysis_modules.print_format import print_format
 plt.style.use("seaborn-darkgrid")
-
-
-class print_format:
-    PURPLE = '\033[95m'
-    CYAN = '\033[96m'
-    DARKCYAN = '\033[36m'
-    BLUE = '\033[94m'
-    GREEN = '\033[92m'
-    YELLOW = '\033[93m'
-    RED = '\033[91m'
-    BOLD = '\033[1m'
-    UNDERLINE = '\033[4m'
-    END = '\033[0m'
-
-    @staticmethod
-    def print_line(num_lines=100):
-        print(print_format.BOLD + print_format.GREEN + "-" * num_lines + print_format.END)
 
 
 class BasicDataChecks:
---
+++ analysis/analysis_modules/print_format.py
@@ -0,0 +1,15 @@
+class print_format:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+    @staticmethod
+    def print_line(num_lines=100):
+        print(print_format.BOLD + print_format.GREEN + "-" * num_lines + print_format.END)
